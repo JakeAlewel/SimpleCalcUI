680307964bd8cbc67dd52f66cf8800f38164e65a
Jacob Alewel <jakealewel@ymail.com>
2015-10-22 18:57:44 -0700

    Added logic for handling count and average

--- SimpleCalcUI/Calculator.swift
+++ SimpleCalcUI/Calculator.swift
@@ -8,18 +8,92 @@
 
 import Foundation
 
-// Business Logic
+// Business Logic - Public
 
 func solveEquation(input: String) -> String {
     let components = inputComponents(input);
-    if hasFactorialInInput(input) {
-        
+    if hasFactorialInComponents(components) {
+        return computeFactorial(components[0]);
+    } else if hasFunctionInComponents(components) {
+        return computeFunction(components);
     }
     print(components);
     return "Huuzahhh"
 }
 
-// Verification - Public 
+// Business Logic - Private
+
+private func convert(incoming: String) -> Double? {
+    let numberFromString = NSNumberFormatter().numberFromString(incoming);
+    if numberFromString == nil {
+        return nil;
+    } else {
+        return numberFromString?.doubleValue;
+    }
+}
+
+private func computeFactorial(stringToCompute: String) -> String {
+    let number = convert(stringToCompute);
+    if number != nil {
+        let roundedNumber = Int(round(number!));
+        if roundedNumber < 0 {
+            return "ERROR";
+        } else if roundedNumber == 0 {
+            return "1";
+        } else {
+            var factorial = 1;
+            for index in 1...roundedNumber {
+                factorial *= index;
+            }
+            return "\(factorial)";
+        }
+    }
+    return "ERROR";
+}
+
+private func computeFunction(components: [String]) -> String {
+    let functionName = firstFunctionInComponents(components);
+    if functionName != nil {
+        let arrayOfNums = arrayOfNumbersWithoutFunctions(components, functionName: functionName!);
+        if functionName!.containsString("Cnt") {
+            return "\(arrayOfNums.count)";
+        } else if functionName!.containsString("Avg") {
+            let sumOfNums = sumFromNumberArray(arrayOfNums);
+            let average = sumOfNums / Double(arrayOfNums.count);
+
+            return "\(average)";
+        } else {
+            return "ERROR";
+        }
+    }
+    return "ERROR";
+}
+
+private func arrayOfNumbersWithoutFunctions(components: [String], functionName: String) -> [Double] {
+    var arrayOfNums: [Double] = [];
+    
+    for (var index = components.count - 1; index >= 0; index--) {
+        let valueAtIndex = components[index];
+        if !valueAtIndex.containsString(functionName) {
+            let convertedValue = convert(valueAtIndex);
+            if convertedValue != nil {
+                arrayOfNums.append(convert(valueAtIndex)!);
+            }
+        }
+    }
+    
+    return arrayOfNums;
+}
+
+private func sumFromNumberArray(numbers: [Double]) -> Double {
+    var sum: Double = 0.0;
+    for number in numbers {
+        sum += number;
+    }
+    return sum;
+}
+
+// Validation - Public
 
 func hasNoInputValues(input: String) -> Bool {
     return inputComponents(input).count <= 0;
@@ -43,18 +117,13 @@ func hasFactorialInInput(input: String) -> Bool {
 func canAddNewFunctionToInput(input: String, newFunction: String) -> Bool {
     if hasFunctionInInput(input) {
         let components = inputComponents(input);
-        let functionAlreadyExistsInInput = functionExistsInComponents(newFunction, components: components);
-        if functionAlreadyExistsInInput {
-            return !newFunction.containsString("Fact");
-        } else {
-            return false;
-        }
+        return canAddAnotherFunctionToComponents(components, newFunction: newFunction);
     } else {
         return true;
     }
 }
 
-// Verification - Private
+// Validation - Private
 
 private let operatorInputs = ["+", "-", "*", "/", "%"];
 private let functionInputs = ["Cnt", "Avg", "Fact"];
@@ -71,12 +140,8 @@ private func hasOperatorInComponents(components: [String]) -> Bool {
 }
 
 private func hasFunctionInComponents(components: [String]) -> Bool {
-    for string in components {
-        if validFunctionStringFromString(string) != nil {
-            return true;
-        }
-    }
-    return false;
+    let foundFunction = firstFunctionInComponents(components);
+    return foundFunction != nil;
 }
 
 private func hasFactorialInComponents(components: [String]) -> Bool {
@@ -87,6 +152,17 @@ private func hasFactorialInComponents(components: [String]) -> Bool {
     }
     return false;
 }
+
+private func canAddAnotherFunctionToComponents(components: [String], newFunction: String) -> Bool {
+    let functionAlreadyExistsInInput = functionExistsInComponents(newFunction, components: components);
+    if functionAlreadyExistsInInput {
+        return !components.last!.containsString(newFunction)
+    } else {
+        return false;
+    }
+}
+
+// Function Helpers - Private
 
 private func validFunctionStringFromString(inputSubString: String) -> String? {
     for functionString in functionInputs {
@@ -107,6 +183,15 @@ private func functionExistsInComponents(newFunction: String, components: [String
         }
     }
     return true;
+}
+
+private func firstFunctionInComponents(components: [String]) -> String? {
+    for string in components {
+        if validFunctionStringFromString(string) != nil {
+            return string;
+        }
+    }
+    return nil;
 }
 
 // Utility - Private
